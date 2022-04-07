aa33dd1f952e5272f5803ef07a457f85a35391d2
Steven Nguyen <stevennguyen901@hotmail.com>
2022-04-06 17:11:05 -0700

    bing

--- storage/app.py
+++ storage/app.py
@@ -178,7 +178,7 @@ def process_messages():
 #     return NoContent, 201
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml",strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml",base_path="/storage",strict_validation=True, validate_responses=True)
 
 if __name__== "__main__":
     t1 = Thread(target=process_messages)
