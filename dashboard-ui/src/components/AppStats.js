f3e19eeddce295a7dd61a43db721bcdd05c2d4c1
Steven Nguyen <stevennguyen901@hotmail.com>
2022-04-07 01:46:09 -0700

    bing bong

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -53,7 +53,7 @@ export default function AppStats() {
 						</tr>
 					</tbody>
                 </table>
-                <h3>Last Updated: {newstats['last_updated']}</h3>
+                <h3>Last Updated: {stats['last_updated']}</h3>
 
             </div>
         )
